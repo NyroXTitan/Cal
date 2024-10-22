12cbf459ead06798c94bf126d7c1fcf694b470d5
NyroXTitan <hayw6121@gmail.com>
2024-10-22 21:00:32 +0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
+import { CalculationService } from './calculation.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrls: ['./app.component.css'],
-  providers: [DecimalPipe]  
+  providers: [DecimalPipe]
 })
 export class AppComponent {
   displayValue: string = '';
 
-  constructor(private decimalPipe: DecimalPipe) {}  
+  constructor(
+    private decimalPipe: DecimalPipe,
+    private calcService: CalculationService
+  ) {}
 
   onButtonClick(value: string) {
     this.displayValue += value;
@@ -26,10 +30,11 @@ export class AppComponent {
   }
 
   onCalculate() {
-    try {
-      const result = eval(this.displayValue); 
+
+    const result = this.calcService.calculate(this.displayValue);
+    if (result !== 'Error') {
       this.displayValue = this.decimalPipe.transform(result, '1.0-0') || '';
-    } catch (error) {
+    } else {
       this.displayValue = 'Error';
     }
   }
